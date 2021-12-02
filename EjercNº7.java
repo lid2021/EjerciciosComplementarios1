691f2d442f478c37e46ee571f5ed24fc4a580ff7
Lidia Zap <tiristar2015@gmail.com>
2021-12-02 02:35:28 -0300

    Update EjercNº7.java

--- EjercNº7.java
+++ EjercNº7.java
@@ -3,26 +3,29 @@ package com.informatorio;
 import java.util.Scanner;
 
 public class Ejercicio7 {
-    static String minAMayuscula(String caracter){
+     public static void main(String[] args){
+      
+        Scanner aMayusc = new Scanner(System.in);
+        System.out.println("Ingrese una palabra en minuscula: ");
+        String palabra = aMayusc.nextLine();
+        aMayusc.close();
+        System.out.println(pasarAMayusc(palabra));
+    }
+    
+    private static String pasarAMayusc(String palabra){
         String result = "";
         char texto = ' ';
-        for (int i = 0; i < caracter.length(); i++) {
-            if(caracter.charAt(i)>='a'&& caracter.charAt(i) <= 'z'){
-                texto = (char)(caracter.charAt(i) - 32);
+        for (int i = 0; i < palabra.length(); i++) {
+            if(palabra.charAt(i)>='a'&& palabra.charAt(i) <= 'z'){
+                texto = (char)(palabra.charAt(i) - 32);
             }
             else{
-                texto = (char)(caracter.charAt(i));
+                texto = (char)(palabra.charAt(i));
             }
             result += texto;
         }
         return result;
     }
-    public static void main(String[] args){
-        String mayuscula;
-        Scanner text = new Scanner(System.in);
-        System.out.println("Ingrese su palabra en minuscula: ");
-        mayuscula = text.nextLine();
-        System.out.println(minAMayuscula(mayuscula));
-    }
+    
 
 }
