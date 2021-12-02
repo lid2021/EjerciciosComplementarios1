0a867c90a7eff28a680943977895f328fb9b1a0b
Lidia Zap <tiristar2015@gmail.com>
2021-12-02 01:15:24 -0300

    Update EjercNº5.java

--- EjercNº5.java
+++ EjercNº5.java
@@ -5,20 +5,21 @@ import java.util.Scanner;
 public class Ejercicio5 {
 
     public static void main(String[] args) {
-        Scanner mult = new Scanner(System.in);
-        int i, num1, num2, producto;
-        producto = 0;
-        System.out.print("Ingresa el valor del primer numero: ");
-        num1 = mult.nextInt();
-        mult.nextLine();
-        System.out.print("Ingresa el valor del segundo numero: ");
-        num2 = mult.nextInt();
-        mult.nextLine();
-        for (i=1; i<=num2; i++) {
-            producto=producto+num1;
-            System.out.println();
-        }
-        System.out.println("El resultado de: " + num1 + " * "+ num2 +" = " + producto);
-    }
+        Scanner numInt = new Scanner(System.in);
+        
+        System.out.println("Ingrese un numero: ");
+        int num1 = numInt.nextInt();
+        System.out.println("Ingrese otro numero: ");
+        int num2 = numInt.nextInt();
+        numInt.close();
 
+        numeroIngresado (num1, num2);
+           
     }
+    public static void numeroIngresado(int num1, int num2){
+        int mult = num1 * num2;
+            
+        System.out.println(num1 + " x " + num2 + " " +"="+ " " +mult);
+        
+    }    
+}
