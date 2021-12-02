f0dc4debc0c2cd3644ff715fd94fd2aa5f3b2897
Lidia Zap <tiristar2015@gmail.com>
2021-12-02 01:51:26 -0300

    Update EjercNº6.java

--- EjercNº6.java
+++ EjercNº6.java
@@ -4,21 +4,19 @@ import java.util.Scanner;
 
 public class Ejercicio6 {
         public static void main(String[] args) {
-            int base;
-            int exponente;
-            int resultado = 1;
+           
+        int resultado = 1;
 
-            Scanner potencia = new Scanner(System.in);
-            System.out.println("Introduzca la base: ");
-            base = potencia.nextInt();
-            potencia.nextLine();
-            System.out.println("Introduzca el exponente: ");
-            exponente = potencia.nextInt();
-            potencia.nextLine();
-            for (int i = 1; i <= exponente; i++) {
-                resultado *= base;
-            }
-            System.out.println(resultado);
+        Scanner potencia = new Scanner(System.in);
+        System.out.println("Introduzca la base: ");
+        int base = potencia.nextInt();
+        System.out.println("Introduzca el exponente: ");
+        int exponente = potencia.nextInt();
+        potencia.close();
+        for (int i = 1; i <= exponente; i++) {
+            resultado *= base;
         }
-
+        System.out.println(base + " elevado a " + exponente + " = " + resultado);
     }
+
+}
